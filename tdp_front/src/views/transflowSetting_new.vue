6e9c05f7cc80f6a424848842e0280a9bc44109f7
mugougao <76206492+mugougao@users.noreply.github.com>
2024-01-19 20:57:50 +0800

    1.1920.57

--- tdp_front/src/views/transflowSetting_new.vue
+++ tdp_front/src/views/transflowSetting_new.vue
@@ -1,21 +1,36 @@
 <template>
     <div id="task_new">
         <div id="taskx">
-            <div id="task_name">
+            <div id="task_name1">
                 <label id="name_lable">任务名称：</label>
-                <input v-model="task_name" id="name_input" />
+                <input  id="name_input" />
             </div>
             <div id="task_url">
-                <label id="url_lable">任务地址：</label>
-                <input v-model="task_name" id="url_input" />
+                <label id="url_lable">输出地址：</label>
+                <div id="jiashurukuang">
+                <input  id="url_input"/>
+                <p id="url1">{{ url }}</p>
+               </div>
             </div>
         </div>
     </div>
 </template>  
   
-<script>
-// import { ref } from 'vue'
-// let task_name=ref('1')
+<script setup>
+ import { ref,onMounted } from 'vue'
+let url= ref(0)
+let ip=window.location.hostname
+let ws=`ws://${ip}:31000/ws/streamer/`
+url.value=ws
+
+onMounted(() => {  
+var url1 = document.querySelector('#url1');  
+var url_input = document.querySelector('#url_input');  
+var url1tWidth = url1.offsetWidth; 
+url_input.style.width = 890-url1tWidth+"px";  
+url_input.style.left = 10+url1tWidth+"px";  
+})
+
 </script>  
   
 <style> 
@@ -32,11 +47,11 @@
 
   }
 
-  #task_name {
+  #task_name1 {
       position: absolute;
       left: 30px;
       top: 15px;
-      width: 400px;
+      width: 1000px;
       height: 36px;
 
   }
@@ -45,7 +60,7 @@
       position: absolute;
       left: 30px;
       top: 70px;
-      width: 400px;
+      width: 1000px;
       height: 36px;
   }
 
@@ -58,36 +73,80 @@
       border-radius: 5px;
       width: 900px;
       margin: auto;
-
+      border-width: 1px;
+      outline: none;
+      color:white;
+      font-size:16px;
+      font-weight: lighter;
+      font-family: Arial, Helvetica, sans-serif;
   }
+
   #url_input {
       position: absolute;
-      height: 30px;
+      height: 28px;
       background-color: #1C1E24;
       border-style: solid;
-      border-color:#705DEB;
+      border-color:#705DEBff;
+      font-family: Arial, Helvetica, sans-serif;
+      top: 2px;
       border-radius: 5px;
-      width: 900px;
       margin: auto;
+      border-width: 0px;
+      outline: none;
+      color:white;
+      font-size:16px;
+      font-weight: lighter;
   }
+
   #name_lable{
+    position: relative;
     color: #ffffff;
     font-size: 18px;
-    top:6px;
+    top:4px;
+    user-select: none;
   }
+
   #url_lable{
+    position: relative;
     color: #ffffff;
     font-size: 18px;
+    top:4px;
+    user-select: none;
   }
+
   #taskx{
     position: absolute;
     border-style: solid;
     border-color:#705DEB;
+    border-width: 2px;
     height:120px;
     width:1479px;
     border-radius: 10px;
-    top: 120px;
-    left: 120px;
+    top: 80px;
+    left: 80px;
+  }
+
+  #url1{
+    position: absolute;
+    top:0px;
+    line-height: 0px;
+    left: 10px;
+    color:#ffffff;
+
+  }
+
+  #jiashurukuang{
+    position: absolute;
+      top: 0px;
+      left: 90px;
+      height: 32px;
+      background-color: #1C1E24;
+      border-style: solid;
+      border-color:#705DEB;
+      border-radius: 5px;
+      margin: auto;
+      border-width: 1px;
+      width: 904px;
   }
 </style>
 
